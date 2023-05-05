1085846d1fb382b44ccfcfecfd272ff7ef7903c2
Alves Bernardo <bernardo.correia.alves@student.nhlstenden.com>
2023-05-05 16:36:45 +0200

    Add clickable Image

--- app/src/main/java/com/example/tileandbackground/MainActivity.kt
+++ app/src/main/java/com/example/tileandbackground/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.tileandbackground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val image_view = findViewById(R.id.drink1) as ImageView
+
+        R.id.drink1.setClickListener{
+            Toast.makeText(this@MainActivity, "You clicked on ImageView.", Toast.LENGTH_SHORT).show()
+        }
+        }
     }
 }
