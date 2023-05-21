692a302c08885b1ffcbf9fe1561a36effcd00c66
AlvesBernardo <bernardo.correia.alves@student.nhlstenden.com>
2023-05-21 19:42:52 +0200

    Add ability to change page

--- app/src/main/java/com/example/tileandbackground/MainActivity.kt
+++ app/src/main/java/com/example/tileandbackground/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.tileandbackground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_rum_coke)
+        setContentView(R.layout.activity_main)
+
+        //get reference to Image
+        val arrow = findViewById(R.id.arrowGoToThirdPage) as ImageView
+        //click Listener
+        arrow.setOnClickListener{
+            //what should it do
+            setContentView(R.layout.activity_main)
         }
     }
+
+}
+
 
--- app/src/main/java/com/example/tileandbackground/Rum_coke.kt
+++ app/src/main/java/com/example/tileandbackground/Rum_coke.kt
@@ -2,10 +2,20 @@ package com.example.tileandbackground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class Rum_coke : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_rum_coke)
+
+        //get reference to Image
+        val backToMain = findViewById(R.id.arrowBackToMain) as ImageView
+        //click Listener
+        backToMain.setOnClickListener{
+            //what should it do
+            setContentView(R.layout.activity_main)
+        }
+
+
     }
 }
