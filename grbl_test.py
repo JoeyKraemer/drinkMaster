3cd76c1d038ae49dd4e5e896d78ec6c086bc43f2
PeterRubint <peter.rubint@student.nhlstenden.com>
2023-05-24 15:42:18 +0200

    new script

--- grbl_test.py
+++ grbl_test.py
@@ -46,17 +46,21 @@ s = serial.Serial('/dev/tty.usbmodem1811',115200)
 f = open('grbl.gcode','r');
 
 # Wake up grbl
-s.write("\r\n\r\n")
+a = "\r\n\r\n"
+s.write(a.encode())
 time.sleep(2)   # Wait for grbl to initialize 
 s.flushInput()  # Flush startup text in serial input
 
 # Stream g-code to grbl
 for line in f:
     l = line.strip() # Strip all EOL characters for consistency
-    print ('Sending: ' + l)
-    s.write(l + '\n') # Send g-code block to grbl
+    print ('Sending: ') 
+    print (l)
+    s.write(l.encode())
+    s.write('\n') # Send g-code block to grbl
     grbl_out = s.readline() # Wait for grbl response with carriage return
-    print(' : ' + grbl_out.strip())
+    print(' : ') 
+    print(grbl_out.strip())
 
 # Wait here until grbl is finished to close serial port and file.
 # raw_input("  Press <Enter> to exit and disable grbl.") 
