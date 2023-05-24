9a889204b01cb474741805a4a99d8ec032812e37
HakanEgeOzerol <111962975+HakanEgeOzerol@users.noreply.github.com>
2023-05-24 12:11:05 +0200

    Styled the popup box ADD

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # drinkMaster
 NHLStenden year 1 period 4 project innovate
+
+Function of app. User selects drink, confirmation button comes up, if user selects yes drink will be prepared. 
+Frontend: Hakan Ege
+Backend: Bernardo
+Raspberry Pi connection: Joey
--- app/src/main/java/com/example/tileandbackground/MainActivity.kt
+++ app/src/main/java/com/example/tileandbackground/MainActivity.kt
@@ -2,19 +2,38 @@ package com.example.tileandbackground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
-import android.widget.Toast
+import android.widget.PopupMenu
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val image_view = findViewById(R.id.drink1) as ImageView
+        //get reference to Image
+        val arrow = findViewById(R.id.arrowChangePage) as ImageView
+        //click Listener
+        arrow.setOnClickListener{
+            //what should it do
+            setContentView(R.layout.activity_rum_coke)
 
-        R.id.drink1.setClickListener{
-            Toast.makeText(this@MainActivity, "You clicked on ImageView.", Toast.LENGTH_SHORT).show()
         }
-        }
+
+
+    }
+
+    private fun showPop(view: View){
+
+        val popUp = PopupMenu(this,view)
+        popUp.inflate(R.menu.popup)
+
     }
 }
+
+
+
