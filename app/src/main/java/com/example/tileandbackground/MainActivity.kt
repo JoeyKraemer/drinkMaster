59ef4e0a5acb64bfc0ec7382aafafad5c14a39ea
Alves Bernardo <bernardo.correia.alves@student.nhlstenden.com>
2023-05-24 11:25:32 +0200

    Add implementaion for charts

--- app/src/main/java/com/example/tileandbackground/MainActivity.kt
+++ app/src/main/java/com/example/tileandbackground/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.tileandbackground
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ContextMenu
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.PopupMenu
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : Activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,13 +27,33 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+
+       var showPopUp = findViewById(R.id.imageButton) as ImageButton
+       var countMocktail = 0
+       var countRumCoke = 0
+       var countLemonade = 0
+       var countCokeLemon = 0
+
+        showPopUp.setOnClickListener{
+            showPop()
+
+        }
+
+
+
+
+
     }
 
-    private fun showPop(view: View){
 
-        val popUp = PopupMenu(this,view)
-        popUp.inflate(R.menu.popup)
+    private fun showPop(){
 
+       val builder = AlertDialog.Builder(this)
+       val customView = LayoutInflater.from(this).inflate(R.layout.pop_up,null)
+       builder.setView(customView)
+       val dialog = builder.create()
+
+        dialog.show()
     }
 }
 
