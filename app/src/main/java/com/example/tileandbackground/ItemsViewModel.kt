a9314fb561c43d8ac820904f1f31009f05ab2d0f
HakanEgeOzerol <111962975+HakanEgeOzerol@users.noreply.github.com>
2023-06-04 18:52:50 +0200

    Added recycler view ADD

---
+++ app/src/main/java/com/example/tileandbackground/ItemsViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.tileandbackground
+
+data class ItemsViewModel(val image: Int, val text: String) {
+}
+
--- app/src/main/java/com/example/tileandbackground/MainActivity.kt
+++ app/src/main/java/com/example/tileandbackground/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.tileandbackground
 
-import android.app.Activity
+import  android.app.Activity
 import android.content.Intent
 import android.os.AsyncTask
 import android.os.Build
