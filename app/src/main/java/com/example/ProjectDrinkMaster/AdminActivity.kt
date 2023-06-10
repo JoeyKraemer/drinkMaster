70b1fd24f973db274a76ca79d7220d47ce41f215
Yohan Lozanov <yohan.lozanov@student.nhlstenden.com>
2023-06-10 13:07:26 +0200

    added password dialog

--- app/src/main/java/com/example/ProjectDrinkMaster/AdminActivity.kt
+++ app/src/main/java/com/example/ProjectDrinkMaster/AdminActivity.kt
@@ -2,11 +2,19 @@ package com.example.ProjectDrinkMaster;
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.AlertDialog
+import androidx.appcompat.app.AlertDialog
 import com.example.ProjectDrinkMaster.R
 import com.example.ProjectDrinkMaster.readRequest
 import kotlin.random.Random
@@ -14,19 +22,31 @@ import kotlin.random.Random
 class AdminActivity : AppCompatActivity() {
     val url = "http://192.168.0.102:5000/"
 
+    private val editTextArray: ArrayList<EditText> = ArrayList(NUM_OF_DIGITS)
+    companion object {
+        const val NUM_OF_DIGITS = 4
+    }
 
     // size of graph in dp
     val graphHeight = 600
 
+    val pincode = 7338
+
+    private lateinit var dialog: AlertDialog
+
+    private var keycodeDigitElements = ArrayList<EditText>(4)
+
     private val graphBarLengths = ArrayList<Int>(4)
 
     private var graphBars = ArrayList<ImageView>(4)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_admin)
 
+        // ===== BUTTONS ======
         /*
         // get buttons
         val rebootPiButton = findViewById<Button>(R.id.rebootPiButton)
@@ -35,29 +55,7 @@ class AdminActivity : AppCompatActivity() {
          */
 
 
-        // dynamically get graph bar images
-        //for(i in graphBars.indices){
-            //graphBars[i] = findViewById<ImageView>(resources.getIdentifier("graphBar$i","id", getPackageName()))
-        //}
-
-        graphBars += findViewById<ImageView>(R.id.drinkBar0)
-        graphBars += findViewById<ImageView>(R.id.drinkBar1)
-        graphBars += findViewById<ImageView>(R.id.drinkBar2)
-        graphBars += findViewById<ImageView>(R.id.drinkBar3)
-
-        // import array of amount of drinks sold from main, if it fails, all values are 0
-        //val importedArray = intent.getIntArrayExtra("drinkValues")
-        //if (importedArray != null) {
-        //    for(i in graphBarLengths.indices){
-        //        graphBarLengths[i] = importedArray[i]
-        //    }
-        //}
-        for(i in (0..3)){  //placeholder
-            graphBarLengths += (0..10).random()
-        }
-
-        //resizeGraph()
-
+        // set listeners for 3 buttons
         /*
         rebootPiButton.setOnClickListener(){
 
@@ -70,7 +68,27 @@ class AdminActivity : AppCompatActivity() {
         }
         */
 
+
+        // ===== KEYCODE ======
+        // create pop up password
+        showPasswordDialog()
+
+
+        // ===== GRAPH ======
+        // randomly create values for graph
+
+        for(i in (0..3)){
+            graphBarLengths += (0..10).random()
+        }
+
+        graphBars += findViewById<ImageView>(R.id.drinkBar0)
+        graphBars += findViewById<ImageView>(R.id.drinkBar1)
+        graphBars += findViewById<ImageView>(R.id.drinkBar2)
+        graphBars += findViewById<ImageView>(R.id.drinkBar3)
+
+        // add values to graph
         resizeGraph()
+
     }
 
     // will change the graph bar image sizes, depending on the values in graphBarLengths (designed in use with "fitXY")
@@ -114,5 +132,98 @@ class AdminActivity : AppCompatActivity() {
     private fun readPage(): String{
         var hello = readRequest(url,"").execute().get()
         return hello
+    }
+
+    private val textWatcher = object : TextWatcher{
+        override fun afterTextChanged(s: Editable?) {
+
+            var numTemp : String
+
+            (0 until keycodeDigitElements.size) // iterates through every value in keycodeDigitSize
+                .forEach { i ->
+
+                    if (s != null) {
+                        if (s.isBlank()) {
+                            return
+                        }
+                    }
+
+                    if (s === keycodeDigitElements[i].editableText) {
+                        if (i != keycodeDigitElements.size - 1) { //not last char
+                            keycodeDigitElements[i + 1].requestFocus()
+                            keycodeDigitElements[i + 1].setSelection(keycodeDigitElements[i + 1].length())
+                            return
+                        } else {
+                            //Last character is inserted
+                            if(validateCode(getCode())){
+                                dialog.hide()
+                            }
+                            else{
+                                for(ii in keycodeDigitElements.indices){
+                                    keycodeDigitElements[ii].setText("")
+                                }
+                                keycodeDigitElements[0].requestFocus()
+                            }
+                        }
+                    }
+                }
+        }
+
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+        }
+    }
+
+    private fun showPasswordDialog(){
+        val builder = AlertDialog.Builder(this)
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.pincode_pop_up, null)
+        builder.setView(dialogView)
+        dialog = builder.create()
+        dialog.setCancelable(false);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show()
+
+        // add keycode digit blocks
+        keycodeDigitElements += dialogView.findViewById<EditText>(R.id.code1)
+        keycodeDigitElements += dialogView.findViewById<EditText>(R.id.code2)
+        keycodeDigitElements += dialogView.findViewById<EditText>(R.id.code3)
+        keycodeDigitElements += dialogView.findViewById<EditText>(R.id.code4)
+
+        //add text change listener
+        for(i in keycodeDigitElements.indices){
+            keycodeDigitElements[i].addTextChangedListener(textWatcher)
+
+            // add backspace key listener
+            keycodeDigitElements[i].setOnKeyListener { _, keyCode, event ->
+                if (keyCode == KeyEvent.KEYCODE_DEL && event.action == KeyEvent.ACTION_DOWN) {
+                    //backspace
+                    if (i != 0) { //Don't implement for first digit
+                        keycodeDigitElements[i - 1].requestFocus()
+                        keycodeDigitElements[i - 1]
+                            .setSelection(keycodeDigitElements[i - 1].length())
+                    }
+                }
+                false
+            }
+        }
+
+        keycodeDigitElements[0].requestFocus()
+    }
+
+    fun getCode() : String{
+        var code = ""
+        for (i in keycodeDigitElements.indices){
+            val digit = keycodeDigitElements[i].text.toString().trim { it <= ' ' }
+            code += digit
+        }
+        return code
+    }
+
+    fun validateCode(code:String) : Boolean{
+        return (code == pincode.toString())
     }
 }
