187cfcbb3aacd241ed759954e9a2b6c05a628796
JoeyKraemer <joey.kramer@student.nhlstenden.com>
2023-06-17 19:29:10 +0200

    add python process for error feed + added calibration cycle + redone index.py

---
+++ flask/grbl_error_log_test.py
@@ -0,0 +1,76 @@
+# Creates html page with error message and soft resets grbl 
+import serial
+import time
+
+from flask import render_template
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        main()
+        return render_template("index.html")
+    elif request.method == "GET":
+        if request.args.get("action"):
+            v = request.args.get("action")
+            if v == "calibration":
+                calibration()
+            elif v == "goToUser":
+                main("goToUser")
+            elif v == "DRINK1":
+                main("DRINKNAME")
+            elif v == "DRINK2":
+                main("DRINKNAME")
+            elif v == "DRINK3":
+                main("DRINKNAME")
+            elif v == "DRINK4":
+                main("DRINKNAME")
+        else:
+            return render_template("index.html")
+
+def showError(error,sp):
+    if error.find("Alarm"):
+        sp.close()
+        return render_template("error.html")
+
+def sendToGRBL(serial,file):
+    a = "\r\n\r\n"
+    serial.write(a.encode())
+    time.sleep(2)   # Wait for grbl to initialize 
+    serial.flushInput()  # Flush startup text in serial input
+
+    for line in file:
+        l = line.strip() # Strip all EOL characters for consistency
+        print ('Sending: ') 
+        print (l)
+        l = l + '\n'
+        serial.write(l.encode())
+        grbl_out = serial.readline().strip # Wait for grbl response with carriage return
+        showError(grbl_out)
+        print('Response: ') 
+        print(grbl_out)
+    serial.close()
+
+def main(drink):
+    s = serial.Serial('/dev/ttyUSB0',115200)
+    f = open('/GCODE/'+ drink +'.gcode','r')
+    sendToGRBL(s,f)
+
+def calibration():
+    s = serial.Serial('/dev/ttyUSB0',115200)
+    f = open('/GCODE/homingY.gcode','r')
+    sendToGRBL(s,f)
+    f = open('/GCODE/homingX.gcode','r')
+    sendToGRBL(s,f)
+    f = open('/GCODE/homingZ.gcode','r')
+    sendToGRBL(s,f)
+    f = open('/GCODE/goToUser.grbl.gcode','r')
+    sendToGRBL(s,f)
+
+if __name__ == "__main__":
+    main()
+
+
