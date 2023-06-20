6c3974f4aa1b7ce7d2cc3899ecb99f05ea272e1e
PeterRubint <peter.rubint@student.nhlstenden.com>
2023-06-20 17:33:35 +0200

    gcode

--- flask/grbl_error_log_test.py
+++ flask/grbl_error_log_test.py
@@ -118,14 +118,8 @@ def calibration():
         f = open('GCODE/pushZ.gcode','r')
         sendToGRBL(s,f,6)
         i = 7
-    
-    if i == 7:
-        s = serial.Serial('/dev/ttyUSB0',115200)
-        f = open('GCODE/pushZ.gcode','r')
-        sendToGRBL(s,f,6)
-        i = 8
 
-    if i == 8:
+    if i == 7:
         s = serial.Serial('/dev/ttyUSB0',115200)
         f = open('GCODE/goToUser.gcode','r')
         sendToGRBL(s,f,10)
