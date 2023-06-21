773d1750c4bf69cddc71c37b0cfc69477ee877cc
PeterRubint <peter.rubint@student.nhlstenden.com>
2023-06-21 19:15:31 +0200

    Drink2 value adjustmen

--- flask/grbl_hope.py
+++ flask/grbl_hope.py
@@ -60,7 +60,7 @@ def action (drink):
 
     freeCup = []
     drink1 = []
-    drink2 = ["$X", "G92 X0 Y0 Z0", "G1 F9000", "G1 Z1900", "G0 F15000", "G0 Y0090", "G0 X0130"]
+    drink2 = ["$X", "G92 X0 Y0 Z0", "G1 F9000", "G1 Z1900", "G0 F15000", "G0 Y0090", "G0 X0110"]
     drink3 = []
     drink4 = []
 
