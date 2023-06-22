5b294896a1313f2aa71ce8332cc3fbfa2d5c11c7
PeterRubint <peter.rubint@student.nhlstenden.com>
2023-06-22 09:43:28 +0200

    Added unlock code to new functions

--- flask/grbl_hope.py
+++ flask/grbl_hope.py
@@ -71,8 +71,8 @@ def action (drink):
     drink3 = [] # Drink 3
     drink4 = [] # Drink 4
 
-    pushUp = ["G92 Z0","G0 Z-0800"]
-    pushDown = ["G92 Z0", "G0 Z800"]
+    pushUp = ["$X","G92 Z0","G0 Z-0800"]
+    pushDown = ["$X", "G92 Z0", "G0 Z0800"]
 
    
     if drink == "calibration":
