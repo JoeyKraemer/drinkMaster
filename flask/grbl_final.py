3e4e9449a13a5152a52f7c7ae761d0528c07a8f7
Yohan Lozanov <yohan.lozanov@student.nhlstenden.com>
2023-06-23 15:36:35 +0200

    moved movetouser, adjusted values

--- flask/grbl_final.py
+++ flask/grbl_final.py
@@ -28,19 +28,15 @@ def index():
                 return render_template("index.html")
             elif v == "DRINK1":
                 action('drink1')
-                goToUser(x,y,z)
                 return render_template("index.html")
             elif v == "DRINK2":
                 action("drink2")
-                goToUser(x,y,z)
                 return render_template("index.html")   
             elif v == "DRINK3":
                 action("drink3")
-                goToUser(x,y,z)
                 return render_template("index.html")
             elif v == "DRINK4":
                 action("drink4")
-                goToUser(x,y,z)
                 return render_template("index.html")
             elif v == "rebootPi":
                 restartPi()
@@ -86,7 +82,7 @@ def action (drink):
     drink3 = [
         "$x", "G90", "G0 F9000", "G0 Z1900", "G0 F15000", "G0 Y0315", "G0 X0110", 
         "G0 F6000", "G92 z0", "G0 Z-1000", "G04 P5", "G92 z0", "G0 Z0900",
-        "G0 F15000", "G92 x0", "G0 X-220", "G92 y0","G0 Y-5","G0 F6000", "G92 z0", "G0 Z-900", "G04 P5", "G92 z0", "G0 Z0800"
+        "G0 F15000", "G92 x0", "G0 X-210", "G92 y0","G0 Y-5","G0 F6000", "G92 z0", "G0 Z-900", "G04 P5", "G92 z0", "G0 Z0800"
     ] # Drink 3
     drink4 = [] # Drink 4
    
@@ -99,14 +95,16 @@ def action (drink):
         print(drink+": Perfectly executed")
     elif drink == "drink1":
         sendToGRBL(drink1)
-        goToUser()
+        goToUser(x,y,z)
     elif drink == "drink2":
         sendToGRBL(drink2)
+        goToUser(x,y,z)
     elif drink == "drink3":
         sendToGRBL(drink3)
-        goToUser()
+        goToUser(x,y,z)
     elif drink == "drink4":
         sendToGRBL(drink4)
+        goToUser(x,y,z)
     elif drink == "pushUp":
         sendToGRBL(pushUp)
     elif drink == "pushDown":
